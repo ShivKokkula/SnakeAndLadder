23e4b204f218df94dfde353680c5f5285212eab0
ShivKokkula <shivbk987@gmail.com>
2021-05-22 21:07:58 +0530

    Refactor - added Dice rolling feature in the program

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,12 +1,23 @@
 package SnakeAndLadder;
 
 public class SnakeAndLadder {
+	
+	int playerOne = 0;
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Snake and Ladder Game");
+		SnakeAndLadder sn = new SnakeAndLadder();
+		for (int i = 0; i < 100; i++) {
+			System.out.println(sn.rollDice());
+		}
 		
-		int playerOne = 0;
+	}
+	
+	public int rollDice() {
+		
+		int diceOutCome = (int) ((Math.floor(Math.random() * 10) % 6)+1);
+		return diceOutCome;
 	}
 
 }
